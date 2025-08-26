0974e3d76e4ed66b83d57280254ef700a105bc6a
akira <akira@10.69.123.90>
2025-08-26 14:27:13 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Argos Zhang (azhang17@huskers.unl.edu) 
+ * Date: 08/26/2025
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author azhang17
+ *
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World, I am Argos!");
+	}
+
+}
